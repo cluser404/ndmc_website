d07445dc443a7f38763760335e208131ad1e6cdf
cluser404 <cluser404@github.com>
2022-03-26 16:47:33 +0600

    added better homepage title

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,7 +12,7 @@
     <!--Parmanent Marker Font-->
     <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
     <!--meta-->
-    <title>NDMC | Homepage</title>
+    <title>Notre Dame Math Club - Official Website</title>
     <!--opengraph meta-->
 	<meta property="og:title" content="Notre Dame Math Club Homepage" />
     <meta property="og:description" content="NDMC is the best Math Club in Bangladesh. Notre Dame welcomed the newest member to its plethora of clubs on the 14th of March, 2017: The Notre Dame Math Club(NDMC).">
